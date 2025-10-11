aac731961fca9f31b8df0ee1796ebf35814d6db2
DominikVelic <dominikvelic@gmail.com>
2025-10-12 01:37:52 +0200

    added textExtendComponent

---
+++ resources/views/components/text-extend.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div>
+        <div class="flex text-white">
+            <div class="trigger">
+                {{ $title }}
+            </div>
+            <div class="bg-white w-[1px] h-full">
+            </div>
+
+            <div id="slide-element" class="slide-element">
+                {{ $description }}
+            </div>
+        </div>
+
+    </div>
+</div>
