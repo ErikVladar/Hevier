f9acc6c6028455d5e6181d0eefca8a1eda58a901
Erik Vladár <125514263+ErikVladar@users.noreply.github.com>
2025-11-10 13:06:04 +0100

    o knihe responzivita

--- resources/views/components/text-extend-left.blade.php
+++ resources/views/components/text-extend-left.blade.php
@@ -14,7 +14,7 @@
 
 <div class="flex space-x-2 md:space-x-8">
     <div
-        class="flex items-center text-black justify-center bg-gradient-to-br from-yellow-200 to-yellow-300  text-xl md:text-4xl content-justify shadow-xl px-8 font-semibold text-center w-48 md:w-72 h-32 flex-shrink-0 rotate-1">
+        class="flex items-center text-black justify-center bg-gradient-to-br from-yellow-200 to-yellow-300  text-md md:text-4xl content-justify shadow-xl px-8 font-semibold text-center w-24 md:w-72 h-24 md:h-32 flex-shrink-0 rotate-1">
 
         <div class="absolute -top-2 w-5 h-5 bg-{{ $color }}-500 rounded-full shadow-md border border-{{ $color }}-700 z-10">
         </div>
@@ -22,7 +22,7 @@
     </div>
 
     <div
-        class="flex items-center text-black justify-center bg-white  py-4 w-32 md:w-[28rem] shadow-xl text-justify text-xs md:text-lg px-2 md:px-8 min-h-36 leading-relaxed -rotate-1">
+        class="flex items-center text-black justify-center bg-white  py-4 w-48 md:w-[28rem] shadow-xl text-justify text-xs md:text-lg px-2 md:px-8 min-h-36 leading-relaxed -rotate-1">
 
         <div class="absolute -top-2 w-5 h-5 bg-{{ $color }}-500 rounded-full shadow-md border border-{{ $color }}-700 z-10">
         </div>
